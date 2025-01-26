603d294b420a7c2e4480603f25036c2354407c3c
Tamta <tamtakopadz16@gmail.com>
2025-01-26 21:38:36 +0400

    Integrate NgRx for state management

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,11 +3,13 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient, withFetch } from '@angular/common/http';
+import { provideStore } from '@ngrx/store';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideHttpClient(withFetch()),
-  ],
+    provideStore()
+],
 };
