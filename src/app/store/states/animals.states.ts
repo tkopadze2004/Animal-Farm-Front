cd4982cc6f6bb6afcd6a05fda6610960d0502315
Tamta <tamtakopadz16@gmail.com>
2025-01-27 20:02:14 +0400

    update state

--- src/app/store/states/animals.states.ts
+++ src/app/store/states/animals.states.ts
@@ -1,7 +1,8 @@
 import { IAnimal } from '../../core/models/animals.model';
 
 export interface IAnimalsState {
-  animals: IAnimal[];
+  animals?: IAnimal[];
+  thanksCount?: number;
   loading: boolean;
   error: string | null;
 }
