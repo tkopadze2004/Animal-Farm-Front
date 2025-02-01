0b565804255039b47694da638e87633bf71ad523
Tamta <tamtakopadz16@gmail.com>
2025-02-01 19:57:32 +0400

    update apiUrl

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:3000/api/',
+  apiUrl: 'https://animal-farm-back-production.up.railway.app/api/',
+  // apiUrl: 'http://localhost:3000/api/',
 };
